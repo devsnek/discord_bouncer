a98ee4c72e0f509f611eb5ea9028beacd6b6c5e3
Gus Caplan <me@gus.host>
2017-07-03 15:58:57 -0500

    add message editing

--- src/api/APIDispatcherEvents.js
+++ src/api/APIDispatcherEvents.js
@@ -13,10 +13,22 @@ module.exports = {
     handler({ client, args }) {
       const channel = client.channels.get(args.channel_id);
       if (!channel) throw new APIError(APIErrors.INVALID_CHANNEL, args.channel_id);
-      return channel.send({
-        content: args.content,
-        embed: args.embed,
-      }).then((m) => transformTextMessage(m));
+      return channel.send(args).then((m) => transformTextMessage(m));
+    },
+  },
+
+  [Events.MESSAGE_EDIT]: {
+    validation: () =>
+      joi.object().keys({
+        channel_id: joi.string().required(),
+        message_id: joi.string().required(),
+      }),
+    handler({ client, args }) {
+      const channel = client.channels.get(args.channel_id);
+      if (!channel) throw new APIError(APIErrors.INVALID_CHANNEL, args.channel_id);
+      return channel.fetchMessage(args.message_id)
+        .then((message) => message.edit(args))
+        .then((m) => transformTextMessage(m));
     },
   },
 };
