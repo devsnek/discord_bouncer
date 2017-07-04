8ab3a73ad50fc1464a02f2457a9698387a735a80
Gus Caplan <me@gus.host>
2017-07-03 20:58:59 -0500

    allow overriding cdn/rest

--- src/discord_bouncer.js
+++ src/discord_bouncer.js
@@ -1,20 +1,24 @@
 #!/usr/bin/env node
 
+const snekparse = require('snekparse');
 const Discord = require('discord.js');
 const API = require('./API');
 
 if (!process.env.NODE_ENV) process.env.NODE_ENV = 'production';
+const argv = snekparse(process.argv);
 
-const client = new Discord.Client();
+const client = new Discord.Client({
+  http: {
+    host: argv.host,
+    cdn: argv.cdn,
+  },
+});
 const api = new API({ client });
 
 process.stdin.setEncoding('utf8');
-
-process.stdin.on('data', (chunk) => {
-  api.handle(chunk);
-});
-
-api.start();
+process.stdin.on('data', (chunk) => api.handle(chunk));
 
 process.on('unhandledRejection', api.handleGlobalRejection.bind(api));
 process.on('uncaughtException', api.handleGlobalException.bind(api));
+
+api.start();
