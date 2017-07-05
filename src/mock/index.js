938d88dbc8902da7cf21d41069794344f0c0bd50
Gus Caplan <me@gus.host>
2017-07-05 17:29:21 -0500

    sharding

--- src/mock/index.js
+++ src/mock/index.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 const MockAPI = require('./MockAPI');
 
 const client = new Discord.Client({
+  shardCount: +process.snekv.shard_count || undefined,
+  shardId: +process.snekv.shard_id || undefined,
   http: {
     api: process.snekv.api_endpoint,
     cdn: process.snekv.cdn_host,
