ea4535b65b18c78cb62c1301293b2882421c08a8
Gus Caplan <me@gus.host>
2017-07-04 22:39:17 -0500

    unlink the sock when finished

--- src/utils/uds.js
+++ src/utils/uds.js
@@ -1,11 +1,13 @@
 require('promise_util');
 const net = require('net');
 const path = require('path');
+const fs = require('fs');
 const { Snowflake } = require('discord.js');
 
 module.exports = (data) => {
   const file = `./${Snowflake.generate()}.sock`;
   const promise = Promise.create();
+
   const server = net.createServer((c) => {
     c.write(JSON.stringify(data));
     c.end();
@@ -19,7 +21,12 @@ module.exports = (data) => {
     server.close();
     if (promise.isPending) promise.resolve(false);
   }, 10e3);
-  promise.then(() => clearTimeout(timeout));
+
+  promise.then(() => {
+    server.close();
+    clearTimeout(timeout);
+    fs.unlink(file, () => {}); // eslint-disable-line no-empty-function
+  });
 
   return { promise, file: path.resolve(file) };
 };
